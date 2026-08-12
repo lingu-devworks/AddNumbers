5ddbad0895cc4a05b04333e76b9f5b06a2fa2118
lingu123 <lingeshkumar1357@gmail.com>
2026-08-12 11:39:41 +0530

    first commit

---
+++ division.java
@@ -0,0 +1,8 @@
+public class division {
+    public static void main(String[] args) {
+        int a=250;
+        int b=13;
+        int sum=a%b;
+        System.out.println("sum=" +sum);
+    }
+}
